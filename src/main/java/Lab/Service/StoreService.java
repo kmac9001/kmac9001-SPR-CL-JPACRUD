e51d36ce29dc41f56aa104cf08f2102440dff422
Kevin McCreary <kevinmccreary72@gmail.com>
2025-01-08 08:03:51 +0000

    completed

--- src/main/java/Lab/Service/StoreService.java
+++ src/main/java/Lab/Service/StoreService.java
@@ -20,14 +20,14 @@ public class StoreService {
      * @return the persisted store
      */
     public Store persistStore(Store store){
-        return null;
+        return storeRepository.save(store);
     }
     /**
      * TODO: get all store entities
      * @return all store entities
      */
     public List<Store> getAllStores(){
-        return null;
+        return storeRepository.findAll();
     }
     /**
      * TODO: given an id of a store, return the store.
@@ -36,13 +36,19 @@ public class StoreService {
      * @return a store entity
      */
     public Store getStoreById(long id){
-        return null;
+        Optional<Store> optionalStore = storeRepository.findById(id);
+        if(optionalStore.isPresent()){
+            return optionalStore.get();
+        }else{
+            return null;
+        }
     }
     /**
      * TODO: given an id of an existing store, delete the store
      */
     public void deleteStore(long id){
 //        code here
+        storeRepository.deleteById(id);
     }
     /**
      * TODO: given an id and some replacement data for a store, overwrite the data of an existing store,
@@ -50,7 +56,17 @@ public class StoreService {
      * @return the updated store entity
      */
     public Store updateStore(long id, Store replacement){
-        return null;
+        Optional<Store> oS = storeRepository.findById(id);
+        if(oS.isPresent()){
+            Store s = oS.get();
+            s.setName(replacement.getName());
+            storeRepository.save(s);
+            return s;
+        }
+        else{
+            return null;
+        }
+        
     }
 
 }
